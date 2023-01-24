ca4f9acbbe99cd9794ac7b8412c143dcb0cbc052
Ryan1806 <121129037+Ryan1806@users.noreply.github.com>
2023-01-24 11:15:59 -0300

    att

--- modelo/script.js
+++ modelo/script.js
@@ -1,8 +1,18 @@
 
 function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML(`Agora são ${hora} horas`)
+    msg.innerText `Agora são ${hora} horas`
+
+    if(hora >= 0 &&  hora < 12) {
+        img.src = 'img-noite2.png'
+    }else if(hora >= 12 && hora < 18) {
+        img.src = 'img-tarde3.png'
+    }else {
+        img.src = 'img-noite2.png'
+    }
+ 
 }
